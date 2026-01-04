10ee49ae7b29ab988f5d9fe0d39aed4113ad83f1
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-04 08:54:58 +0300

    Create set.js

---
+++ set.js
@@ -0,0 +1,11 @@
+ Application Logs
+2026-01-04T05:28:22.755472+00:00 app[web.1]:   address: '/app/database.db/.s.PGSQL.5432'
+2026-01-04T05:28:22.755472+00:00 app[web.1]: }
+2026-01-04T05:28:22.755473+00:00 app[web.1]: 
+2026-01-04T05:28:22.755473+00:00 app[web.1]: Node.js v24.12.0
+2026-01-04T05:28:22.805117+00:00 heroku[web.1]: Process exited with status 1
+2026-01-04T05:28:22.827863+00:00 heroku[web.1]: State changed from starting to crashed
+2026-01-04T05:28:27.000000+00:00 app[api]: Build succeeded
+2026-01-04T05:28:37.687167+00:00 heroku[router]: at=error code=H10 desc="App crashed" method=GET path="/" host=nyoni-fb-89ca158c920a.herokuapp.com request_id=141baa0d-c69f-ab35-1129-3d6af7279be0 fwd="" dyno= connect=0ms service=0ms status=503 bytes=567 protocol=http1.1 tls=true tls_version=unknown
+2026-01-04T05:28:41.783800+00:00 heroku[router]: sock=client at=warning code=H27 desc="Client Request Interrupted" method=GET path="/favicon.ico" host=nyoni-fb-89ca158c920a.herokuapp.com request_id=6c6b45e7-6a55-d481-d0e7-1918b3c795be fwd="" dyno= connect=0ms service=0ms status=499 bytes=567 protocol=http1.1 tls=true tls_version=unknown
+2026-01-04T05:28:59.404743+00:00 app[api]: Log session created by user devnyoni@gmail.com
